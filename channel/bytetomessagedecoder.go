eb298abd836c001d173f34167003cab50e3cbf04
Yeti Sno <yeti@yetiz.org>
2021-08-31 22:22:11 +0800

    init

---
+++ channel/bytetomessagedecoder.go
@@ -0,0 +1,36 @@
+package channel
+
+import (
+	"github.com/kklab-com/gone-core/utils"
+	"github.com/kklab-com/goth-kkutil/buf"
+	"github.com/kklab-com/goth-kkutil/structs"
+)
+
+type MessageDecoder interface {
+	Decode(ctx HandlerContext, in buf.ByteBuf, out structs.Queue)
+}
+
+type ByteToMessageDecoder struct {
+	DefaultHandler
+	Decode func(ctx HandlerContext, in buf.ByteBuf, out structs.Queue)
+}
+
+func (h *ByteToMessageDecoder) Added(ctx HandlerContext) {
+	if h.Decode == nil {
+		h.Decode = h.decode
+	}
+}
+
+func (h *ByteToMessageDecoder) Read(ctx HandlerContext, obj interface{}) {
+	out := &utils.Queue{}
+	h.Decode(ctx, obj.(buf.ByteBuf), out)
+	for elem := out.Pop(); elem != nil; elem = out.Pop() {
+		ctx.FireRead(elem)
+	}
+
+	ctx.FireReadCompleted()
+}
+
+func (h *ByteToMessageDecoder) decode(ctx HandlerContext, in buf.ByteBuf, out structs.Queue) {
+	out.Push(in)
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/kklab-com/gone-core
+
+go 1.16
+
+require (
+	github.com/google/uuid v1.1.1
+	github.com/kklab-com/goth-base62 v1.0.0
+	github.com/kklab-com/goth-kklogger v1.0.0
+	github.com/kklab-com/goth-kkutil v1.0.2
+	github.com/kklab-com/goth-panic v1.0.1
+	github.com/pkg/errors v0.8.0
+)
